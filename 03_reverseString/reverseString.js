9e91192b25f7b988d7fae4e4e354b05e18e5b9eb
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 16:36:11 +0530

    03 NODE DONE

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let s='';
+    for(let i=0;i<string.length;i++)
+    {
+        s=string[i]+s;
+    }
+    return s;
 };
 
 // Do not edit below this line
