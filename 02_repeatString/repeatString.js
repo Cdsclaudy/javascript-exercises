e5ecdaaaaff5285dfdef6e4eb884992974e5608b
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 16:29:58 +0530

    02 NODE DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if(num<0)
+    return("ERROR");
+    let s1='';
+for(let i=0;i<num;i++)
+{
+    s1=s1+string;
+}
+return s1;
 };
 
 // Do not edit below this line
