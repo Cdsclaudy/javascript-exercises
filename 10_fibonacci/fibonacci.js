89032bdfb5c03e2f6635ab8ce389fa41b1c59544
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 21:24:17 +0530

    10 NODE DONE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+let b=1;
+let c=1;
+if(a<0)
+{
+    return("OOPS");
+}
+if(a==1||a==2)
+    return b;
+else
+{
+    let res=0;
+    for(let i=0;i<a-2;i++)
+    {
+        res=b+c;
+        b=c;
+        c=res;
+    }
+    return res;
+}
 };
 
 // Do not edit below this line
