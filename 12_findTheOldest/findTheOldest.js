6bd257915383d49445bac2fdfd1e135231247a84
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 21:56:32 +0530

    12 NODE DONE

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+let m=0;
+for(let i=1;i<a.length;i++)
+{
+    if(typeof(a[i].yearOfDeath)=="undefined")
+    {
+        a[i].yearOfDeath=new Date().getFullYear();
+    }
+    if(typeof(a[m].yearOfDeath)=="undefined")
+    {
+        a[m].yearOfDeath=new Date().getFullYear();
+    }
+    if(typeof(a[m].yearOfDeath)!="undefined"&&a[i].yearOfDeath-a[i].yearOfBirth>a[m].yearOfDeath-a[m].yearOfBirth)
+    {
+        m=i;
+    }
+}
+return a[m];
 };
 
 // Do not edit below this line
