f554251132dec1dc7aff2527b14554a3a435c68e
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 21:06:01 +0530

    08 NODE DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return(a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return(a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let b=0;
+	for(let i=0;i<a.length;i++)
+  {
+    b+=a[i];
+  }
+  return b;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let b=1;
+	for(let i=0;i<a.length;i++)
+  {
+    b*=a[i];
+  }
+  return b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let p=1;
+  for(let i=0;i<b;i++)
+  {
+    p=p*a;
+  }
+  return p;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let p=1;
+  for(let i=1;i<=a;i++)
+  {
+    p=p*i;
+  }
+  return p;
 };
 
 // Do not edit below this line
