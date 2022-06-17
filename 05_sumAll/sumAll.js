1f36ee2b2367dab841a7cacdba97b32839d3b7f0
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 17:19:49 +0530

    05 NODE DONE

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(typeof(a)!="number"||typeof(b)!="number" || a<0 || b<0)
+    {
+        return("ERROR");
+    }
+let v=0;
+if(a<b)
+{
+for(let i=a;i<=b;i++)
+{
+    v+=i;
+}
+}
+else
+{
+    for(let i=b;i<=a;i++)
+{
+    v+=i;
+}
+}
+return v;
 };
 
 // Do not edit below this line
