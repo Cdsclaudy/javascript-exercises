1ace783d473fa538a44d662641634a7a349bc50a
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 21:15:33 +0530

    09 NODE DONE

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+let rev='';
+let norm='';
+for(let i=0;i<a.length;i++)
+{
+    if(a[i].toLowerCase()>='a'&&a[i].toLowerCase()<='z')
+    {
+        norm=norm+a[i].toLowerCase();
+        rev=a[i].toLowerCase()+rev;
+    }
+}
+if(norm==rev)
+    return true;
+else
+    return false;
 };
 
 // Do not edit below this line
