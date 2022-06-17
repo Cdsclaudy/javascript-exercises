06a679f0c619a05a410b7f701a7ab5f37c3521f4
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-17 17:08:17 +0530

    04 NODE DONE

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
 const removeFromArray = function() {
-
+let arr=arguments[0];
+let b=[];
+for(let i=0;i<arr.length;i++)
+{
+    let a=0;
+    for(let j=1;j<arguments.length;j++)
+    {
+        if(arguments[j]===arr[i])
+        {
+            a=1;
+            break;
+        }
+    }
+    if(a!=1)
+    {   
+        b.push(arr[i]);
+    }
+}
+return b;
 };
 
 // Do not edit below this line
