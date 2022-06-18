21e88fd5f7a2784e27c856b01fb1ba5a4551f3cf
cdsclaudy <claudiusjdsouza@gmail.com>
2022-06-18 13:17:05 +0530

    All done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,37 @@
-const caesar = function() {
-
+const caesar = function(a,b) {
+let c="";
+for(let i=0;i<a.length;i++)
+{
+    if(a.charCodeAt(i)>=97&&a.charCodeAt(i)<=122)
+    {
+        c+=String.fromCharCode(a.charCodeAt(i)+b);
+        if(c.charCodeAt(i)>122)
+        {
+            c[i]=String.fromCharCode(97+c.charCodeAt(i)-122-1);
+        }
+        if(c.charCodeAt(i)<97)
+        {
+            c[i]=String.fromCharCode(122-97-c.charCodeAt(i)+1);
+        }
+    }
+    else if(a.charCodeAt(i)>=65&&a.charCodeAt(i)<=90)
+    {
+        c+=String.fromCharCode(a.charCodeAt(i)+b);
+        if(c.charCodeAt(i)>90)
+        {
+            c[i]=String.fromCharCode(65+c.charCodeAt(i)-90-1);
+        }
+        if(c.charCodeAt(i)<65)
+        {
+            c[i]=String.fromCharCode(90-65-c.charCodeAt(i)+1);
+        }
+    }
+    else
+    {
+        c+=a[i];
+    }
+}
+return c;
 };
 
 // Do not edit below this line
